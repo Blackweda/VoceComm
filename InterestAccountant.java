f1d64be56996e1df16d27137f29e5339341a1d01
Wayne Williams <siloaman@hotmail.ca>
2017-10-29 18:51:29 +1300

    Update InterestAccountant.java

--- InterestAccountant.java
+++ InterestAccountant.java
@@ -1,1 +1,57 @@
+package com.holdings.siloaman.talktoiea;
 
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class InterestAccountant extends AppCompatActivity {
+   
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardinal_direction);
+
+        // SET UP SENSORS
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        // DIRECTION DISPLAY
+        final TextView DirectionTV = (TextView)findViewById(R.id.directionTextView);
+        Button directionButton = (Button)findViewById(R.id.directionButton);
+
+        directionButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+
+                
+            }
+        });
+
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+   
+    
+}
