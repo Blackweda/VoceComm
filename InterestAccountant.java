c1426aa4b1d254c3dedb0f5a937c6f593ded161b
Wayne Williams <siloaman@hotmail.ca>
2017-10-29 19:04:51 +1300

    Update InterestAccountant.java

--- InterestAccountant.java
+++ InterestAccountant.java
@@ -20,16 +20,9 @@ public class InterestAccountant extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardinal_direction);
 
-        // SET UP SENSORS
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        // DIRECTION DISPLAY
-        final TextView DirectionTV = (TextView)findViewById(R.id.directionTextView);
-        Button directionButton = (Button)findViewById(R.id.directionButton);
-
-        directionButton.setOnClickListener(new View.OnClickListener() {
+       
+        Button calculateButton = (Button)findViewById(R.id.);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v){
 
@@ -42,13 +35,12 @@ public class InterestAccountant extends AppCompatActivity {
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+        
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
+        
     }
 
 
